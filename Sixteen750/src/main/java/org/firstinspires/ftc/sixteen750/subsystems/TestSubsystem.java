75a03e5074154d51a6a562e087b783e9a786741b
FTC16750 <technototes@outlook.com>
2025-10-21 17:56:38 -0700

    10/21 doing some stuff with motor velocity

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/subsystems/TestSubsystem.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/subsystems/TestSubsystem.java
@@ -5,20 +5,30 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.technototes.library.hardware.motor.CRServo;
 import com.technototes.library.hardware.motor.EncodedMotor;
 import com.technototes.library.hardware.servo.Servo;
+import com.technototes.library.logger.Log;
 import com.technototes.library.logger.Loggable;
 
 import org.firstinspires.ftc.sixteen750.Hardware;
+import org.firstinspires.ftc.sixteen750.Robot;
 import org.firstinspires.ftc.sixteen750.Setup;
+import org.firstinspires.ftc.sixteen750.commands.TeleCommands;
 
 @Configurable
 public class TestSubsystem implements Loggable {
 
+    @Log.Number(name = "Motor Power")
+    public static double MOTOR_POWER = 0.65; // 0.5 1.0
+
+    public static double TARGET_LAUNCH_VELOCITY = 6000;
+    @Log.Number(name = "Motor Velocity")
+    public static double CURRENT_LAUNCH_VELOCITY = 0.0;
     public static double TESTMOTOR_VELOCITY = 0.25; // 0.5 1.0
 
     public static double TESTCRSERVO_SPEED = 1; // 0.15 0.25
     public static double SERVO_POS = 0.1;
 
     boolean hasHardware;
+    public Robot robot;
     EncodedMotor<DcMotorEx> testmotor;
     CRServo testcrservo;
 
@@ -60,6 +70,22 @@ public class TestSubsystem implements Loggable {
         if (hasHardware) {
             testmotor.setVelocity(0);
             testcrservo.setPower(0);
+        }
+    }
+    public void setMotorVelocityTest(){
+        testmotor.setVelocity(TARGET_LAUNCH_VELOCITY);
+    }
+    public void setMotorPowerTest(){
+        testmotor.setPower(MOTOR_POWER);
+        CURRENT_LAUNCH_VELOCITY = getMotor1Velocity();
+    }
+    public double getMotor1Velocity(){
+        return testmotor.getVelocity();
+    }
+
+    public void VelocityShoot(){
+        if (getMotor1Velocity() == TARGET_LAUNCH_VELOCITY){
+            TeleCommands.GateDown(robot);
         }
     }
 }
