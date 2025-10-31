eb2e808a69b1a17a64f57e4c41ae793b727dc7cb
Kevin Frei <kevinfrei@hotmail.com>
2025-10-31 09:05:09 -0700

    Got the start of the PedroPath config stuff back up

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/AutoConstants.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/AutoConstants.java
@@ -1,12 +1,7 @@
 package org.firstinspires.ftc.sixteen750;
 
-import static org.firstinspires.ftc.sixteen750.Setup.HardwareNames.FL_DRIVE_MOTOR;
-import static org.firstinspires.ftc.sixteen750.Setup.HardwareNames.FR_DRIVE_MOTOR;
-import static org.firstinspires.ftc.sixteen750.Setup.HardwareNames.OTOS;
-import static org.firstinspires.ftc.sixteen750.Setup.HardwareNames.RL_DRIVE_MOTOR;
-import static org.firstinspires.ftc.sixteen750.Setup.HardwareNames.RR_DRIVE_MOTOR;
-
 import com.acmerobotics.dashboard.config.Config;
+import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.control.FilteredPIDFCoefficients;
 import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
@@ -23,83 +18,114 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.sixteen750.Setup.HardwareNames;
 
 @Config
 public class AutoConstants {
 
-    // note these need to be tuned
+    // note these need to be measured:
     public static double botWeightKg = 9.44;
+    public static double robotLength = 10.28; // This is clearly wrong
+    public static double robotWidth = 7.625; // This is also wrong
+
+    // These come from Tuners:
     public static double xvelocity = 10.0;
     public static double yvelocity = 10.0;
-    // Need to talk about naming constants with students:
-    public static double fwdDeceleration = 0.0;
-    public static double latDeceleration = 0.0;
-    public static double fwdTicksToInches = 0.008;
-    public static double strafeTicksToInches = -0.009;
-    public static double turnTicksToInches = 0.018;
-    public static double robotLength = 10.28;
-    public static double robotWidth = 7.625;
-    public static SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(1.50, 0.0, 180);
-    public static double linearscalar = -1.08; //1.9
-    public static double angularscalar = 0.9;
+    public static double fwdDeceleration = -1.0;
+    public static double latDeceleration = -1.0;
+    public static double centripetalScaling = 0.005;
+
+    // These are hand tuned to work how we want
+    public static double brakingStrength = 0.5;
+    public static double brakingStart = 0.5;
+    public static double TValueConstraint = 0.99;
+    public static double timeoutConstraint = 100;
     public static PIDFCoefficients headingPIDF = new PIDFCoefficients(0.0003, 0, 0, 0);
     public static PIDFCoefficients translationPIDF = new PIDFCoefficients(0.0003, 0, 0, 0);
-    public static PIDFCoefficients secondaryheadingPIDF = new PIDFCoefficients(0.0003, 0, 0, 0);
-    public static PIDFCoefficients secondarytranslationPIDF = new PIDFCoefficients(0.0003, 0, 0, 0);
+    // public static PIDFCoefficients secondaryheadingPIDF = new PIDFCoefficients(0.0003, 0, 0, 0);
+    // public static PIDFCoefficients secondarytranslationPIDF = new PIDFCoefficients(0.0003, 0, 0, 0);
     public static FilteredPIDFCoefficients drivePIDF = new FilteredPIDFCoefficients(
         0.025,
         0,
         0.00001,
-        0.6,
+        0.6, // Kalman filter: 60% of D will come from the *previous* derivative
         0.01
     );
 
     //public static FilteredPIDFCoefficients drivePIDF = new FilteredPIDFCoefficients(0.1, 0, 0, 0.01);
     //public static PIDFCoefficients centripetalPIDF = new PIDFCoefficients(0.1, 0, 0, 0.01);
 
-    public static double TValueConstraint = 0.99;
-    public static double timeoutConstraint = 100;
-    public static double brakingStrength = 1.0;
-    public static double brakingStart = 1.0;
+    @Configurable
+    public static class DriveEncoderConfig {
 
-    // Need to explain SIOF to students.
-    // (Hurray for programming languages never quite doing what you expect)
+        public static double fwdTicksToInches = 0.008;
+        public static double strafeTicksToInches = -0.009;
+        public static double turnTicksToInches = 0.018;
+    }
+
+    @Configurable
+    public static class OTOSConfig {
+
+        public static SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(1.50, 0.0, 180);
+        public static double linearscalar = -1.08; //1.9
+        public static double angularscalar = 0.9;
+    }
+
+    @Configurable
+    public static class TwoWheelConfig {
+
+        public static String forwardName = HardwareNames.ODORL;
+        public static String strafeName = HardwareNames.ODOFB;
+        public static double forwardTicksToInches = 5.47; // 5.42, 5.47, 5.49
+        public static double strafeTicksToInches = 5.38; // 5.37, 5.39, 5.38
+        public static double forwardPodYOffset = 4.27;
+        public static double strafePodXOffset = -4.006;
+        public static boolean forwardReversed = true;
+        public static boolean strafeReversed = false;
+        public static RevHubOrientationOnRobot.LogoFacingDirection logoDir =
+            RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
+        public static RevHubOrientationOnRobot.UsbFacingDirection usbDir =
+            RevHubOrientationOnRobot.UsbFacingDirection.DOWN;
+    }
+
     public static DriveEncoderConstants getEncoderConstants() {
         return new DriveEncoderConstants()
-            .forwardTicksToInches(fwdTicksToInches)
-            .strafeTicksToInches(strafeTicksToInches)
-            .turnTicksToInches(turnTicksToInches)
+            .forwardTicksToInches(DriveEncoderConfig.fwdTicksToInches)
+            .strafeTicksToInches(DriveEncoderConfig.strafeTicksToInches)
+            .turnTicksToInches(DriveEncoderConfig.turnTicksToInches)
             .robotLength(robotLength)
             .robotWidth(robotWidth)
-            .rightFrontMotorName(FR_DRIVE_MOTOR)
-            .rightRearMotorName(RR_DRIVE_MOTOR)
-            .leftRearMotorName(RL_DRIVE_MOTOR)
-            .leftFrontMotorName(FL_DRIVE_MOTOR)
+            .rightFrontMotorName(HardwareNames.FR_DRIVE_MOTOR)
+            .rightRearMotorName(HardwareNames.RR_DRIVE_MOTOR)
+            .leftRearMotorName(HardwareNames.RL_DRIVE_MOTOR)
+            .leftFrontMotorName(HardwareNames.FL_DRIVE_MOTOR)
             .leftFrontEncoderDirection(Encoder.FORWARD)
             .leftRearEncoderDirection(Encoder.REVERSE)
             .rightFrontEncoderDirection(Encoder.REVERSE)
             .rightRearEncoderDirection(Encoder.FORWARD);
     }
 
-    public static TwoWheelConstants localizerConstants = new TwoWheelConstants()
-        .forwardEncoder_HardwareMapName(Setup.HardwareNames.ODORL) //odo name
-        .strafeEncoder_HardwareMapName(Setup.HardwareNames.ODOFB) //odo name
-        .IMU_HardwareMapName(Setup.HardwareNames.EXTERNAL_IMU)
-        .forwardPodY(4.27) //offset
-        .strafePodX(-4.006) //offset
-        .forwardTicksToInches(5.47) //5.42, 5.47, 5.49
-        .strafeTicksToInches(5.38) //5.37, 5.39, 5.38
-        .forwardEncoderDirection(Encoder.REVERSE)
-        .strafeEncoderDirection(Encoder.FORWARD)
-        .IMU_Orientation(
-            new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-                RevHubOrientationOnRobot.UsbFacingDirection.DOWN
+    public static TwoWheelConstants getTwoWheelLocalizerConstants() {
+        return new TwoWheelConstants()
+            .forwardEncoder_HardwareMapName(TwoWheelConfig.forwardName)
+            .strafeEncoder_HardwareMapName(TwoWheelConfig.strafeName)
+            .IMU_HardwareMapName(Setup.HardwareNames.EXTERNAL_IMU)
+            .forwardPodY(TwoWheelConfig.forwardPodYOffset)
+            .strafePodX(TwoWheelConfig.strafePodXOffset)
+            .forwardTicksToInches(TwoWheelConfig.forwardTicksToInches)
+            .strafeTicksToInches(TwoWheelConfig.strafeTicksToInches)
+            .forwardEncoderDirection(
+                TwoWheelConfig.forwardReversed ? Encoder.REVERSE : Encoder.FORWARD
             )
-        );
+            .strafeEncoderDirection(
+                TwoWheelConfig.strafeReversed ? Encoder.REVERSE : Encoder.FORWARD
+            )
+            .IMU_Orientation(
+                new RevHubOrientationOnRobot(TwoWheelConfig.logoDir, TwoWheelConfig.usbDir)
+            );
+    }
 
     public static FollowerConstants getFollowerConstants() {
         // tune these
@@ -110,9 +136,10 @@ public class AutoConstants {
             .holdPointTranslationalScaling(1)
             .headingPIDFCoefficients(headingPIDF)
             .drivePIDFCoefficients(drivePIDF)
-            .secondaryHeadingPIDFCoefficients(secondaryheadingPIDF)
-            .secondaryTranslationalPIDFCoefficients(secondarytranslationPIDF)
-            .translationalPIDFCoefficients(translationPIDF);
+            // .secondaryHeadingPIDFCoefficients(secondaryheadingPIDF)
+            // .secondaryTranslationalPIDFCoefficients(secondarytranslationPIDF)
+            .translationalPIDFCoefficients(translationPIDF)
+            .centripetalScaling(centripetalScaling);
     }
 
     public static PathConstraints getPathConstraints() {
@@ -127,10 +154,10 @@ public class AutoConstants {
     public static MecanumConstants getDriveConstants() {
         return new MecanumConstants()
             .maxPower(1)
-            .leftFrontMotorName(FL_DRIVE_MOTOR)
-            .leftRearMotorName(RL_DRIVE_MOTOR)
-            .rightFrontMotorName(FR_DRIVE_MOTOR)
-            .rightRearMotorName(RR_DRIVE_MOTOR)
+            .leftFrontMotorName(HardwareNames.FL_DRIVE_MOTOR)
+            .leftRearMotorName(HardwareNames.RL_DRIVE_MOTOR)
+            .rightFrontMotorName(HardwareNames.FR_DRIVE_MOTOR)
+            .rightRearMotorName(HardwareNames.RR_DRIVE_MOTOR)
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
@@ -141,23 +168,25 @@ public class AutoConstants {
 
     public static OTOSConstants getOTOSConstants() {
         return new OTOSConstants()
-            .hardwareMapName(OTOS)
+            .hardwareMapName(HardwareNames.OTOS)
             .linearUnit(DistanceUnit.INCH)
             .angleUnit(AngleUnit.RADIANS)
             // need to tune for OTOS localization
-            .linearScalar(linearscalar)
-            .angularScalar(angularscalar);
+            .linearScalar(OTOSConfig.linearscalar)
+            .angularScalar(OTOSConfig.angularscalar);
     }
 
     public static Follower createFollower(HardwareMap hardwareMap) {
-        SparkFunOTOS otos = hardwareMap.get(SparkFunOTOS.class, OTOS);
-        otos.calibrateImu();
+        if (Setup.Connected.OTOS) {
+            SparkFunOTOS otos = hardwareMap.get(SparkFunOTOS.class, HardwareNames.OTOS);
+            otos.calibrateImu();
+        }
         return new FollowerBuilder(getFollowerConstants(), hardwareMap)
             .pathConstraints(getPathConstraints())
+            //.driveEncoderLocalizer(getEncoderConstants())
             //.OTOSLocalizer(getOTOSConstants())
             .mecanumDrivetrain(getDriveConstants())
-            .twoWheelLocalizer(localizerConstants)
-            //.driveEncoderLocalizer(getEncoderConstants())
+            .twoWheelLocalizer(getTwoWheelLocalizerConstants())
             .build();
     }
 
