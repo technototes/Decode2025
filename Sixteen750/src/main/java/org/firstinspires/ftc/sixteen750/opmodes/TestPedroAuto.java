af0ba8428f5b7f8bcfffa3f7cebe027907671604
Kevin Frei <kevinfrei@hotmail.com>
2025-11-01 09:54:54 -0700

    Got the test pedro auto moved to 16750

---
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmodes/TestPedroAuto.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.sixteen750.opmodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.technototes.library.command.CommandScheduler;
+import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.library.structure.CommandOpMode;
+import com.technototes.library.util.Alliance;
+import org.firstinspires.ftc.sixteen750.Hardware;
+import org.firstinspires.ftc.sixteen750.Robot;
+import org.firstinspires.ftc.sixteen750.commands.PedroPathCommand;
+import org.firstinspires.ftc.sixteen750.commands.auto.TestPaths;
+import org.firstinspires.ftc.sixteen750.controls.DriverController;
+import org.firstinspires.ftc.sixteen750.helpers.HeadingHelper;
+import org.firstinspires.ftc.sixteen750.helpers.StartingPosition;
+
+@Autonomous(name = "TestPedro", preselectTeleOp = "OneDriver")
+@SuppressWarnings("unused")
+public class TestPedroAuto extends CommandOpMode {
+
+    public Robot robot;
+    public DriverController controls;
+    public Hardware hardware;
+
+    @Override
+    public void uponInit() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        hardware = new Hardware(hardwareMap);
+        robot = new Robot(hardware, Alliance.RED, StartingPosition.Net);
+        TestPaths p = new TestPaths(robot.follower);
+        robot.follower.setStartingPose(p.getStart());
+        CommandScheduler.scheduleForState(
+                new SequentialCommandGroup(
+                        new PedroPathCommand(robot.follower, p.Path1),
+                        // new WaitCommand(0.5),
+                        new PedroPathCommand(robot.follower, p.Path2),
+                        // new WaitCommand(1),
+                        new PedroPathCommand(robot.follower, p.Path3),
+                        // new WaitCommand(2),
+                        new PedroPathCommand(robot.follower, p.Path4),
+                        // new WaitCommand(4),
+                        new PedroPathCommand(robot.follower, p.Path5),
+                        CommandScheduler::terminateOpMode
+                ),
+                OpModeState.RUN
+        );
+    }
+
+    public void uponStart() {
+        robot.prepForStart();
+    }
+
+    public void end() {
+        HeadingHelper.savePose(robot.follower.getPose());
+    }
+}
