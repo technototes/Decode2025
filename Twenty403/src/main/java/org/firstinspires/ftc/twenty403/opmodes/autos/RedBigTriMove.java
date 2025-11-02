85cc79e669168d3f480de427e0b56ffe240f628a
Kooolpool <181432562+koolpoolo@users.noreply.github.com>
2025-11-01 17:08:59 -0700

    added all the move autos so we can get those points not tested though either test at lm1, don't use these or just hope they work lol

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/opmodes/autos/RedBigTriMove.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/opmodes/autos/RedBigTriMove.java
@@ -1,30 +1,27 @@
-package org.firstinspires.ftc.twenty403.opmodes;
+package org.firstinspires.ftc.twenty403.opmodes.autos;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.geometry.BezierCurve;
-import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.technototes.library.command.CommandScheduler;
-import com.technototes.library.command.SequentialCommandGroup;
 import com.technototes.library.structure.CommandOpMode;
 import com.technototes.library.util.Alliance;
+
 import org.firstinspires.ftc.twenty403.AutoConstants;
 import org.firstinspires.ftc.twenty403.Hardware;
 import org.firstinspires.ftc.twenty403.Robot;
 import org.firstinspires.ftc.twenty403.Setup;
-import org.firstinspires.ftc.twenty403.commands.EZCmd;
-import org.firstinspires.ftc.twenty403.commands.auto.Paths;
 import org.firstinspires.ftc.twenty403.helpers.StartingPosition;
+
 @Configurable
-@Autonomous(name = "BlueSmallToBlueGoal", preselectTeleOp = "Two Controller Drive \uD83D\uDDFF")
+@Autonomous(name = "RedBigTriMove", preselectTeleOp = "Two Controller Drive \uD83D\uDDFF")
 @SuppressWarnings("unused")
-public class BlueSmallToBlueGoal extends CommandOpMode {
+public class RedBigTriMove extends CommandOpMode {
 
     public Robot robot;
     public Hardware hardware;
@@ -45,13 +42,17 @@ public class BlueSmallToBlueGoal extends CommandOpMode {
         SparkFunOTOS otos = hardwareMap.get(SparkFunOTOS.class, Setup.HardwareNames.OTOS);
         otos.calibrateImu();
        robot.follower = AutoConstants.createFollower(hardwareMap);
-       robot.follower.setStartingPose(new Pose(80.889, 9.244, 90));
+       robot.follower.setStartingPose(new Pose(109.867, 134.578, 270));
         bluesmalltobluegoal = robot.follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(80.889, 9.244,90), new Pose(107.733, 9.600,90))
+                        new BezierCurve(
+                                new Pose(109.867, 134.578),
+                                new Pose(85.333, 61.867),
+                                new Pose(82.667, 59.200)
+                        )
                 )
-                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(90))
+                .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(0))
                 .build();
 //        robot.follower.setStartingPose(robot.follower.getPose());
 //        bluesmalltobluegoal = robot.follower
@@ -73,7 +74,6 @@ public class BlueSmallToBlueGoal extends CommandOpMode {
     @Override
     public void initLoop(){
         telemetry.addData("Pose:", robot.follower.getPose());
-        robot.follower.setPose(new Pose(80.889, 9.244, 90));
         robot.follower.update();
     }
     @Override
