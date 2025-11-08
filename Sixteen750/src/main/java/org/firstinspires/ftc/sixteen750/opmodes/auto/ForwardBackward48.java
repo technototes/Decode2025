b9d6d7ee631865c216924dc0ce30361564119876
Kevin Frei <kevinfrei@hotmail.com>
2025-11-08 11:13:49 -0800

    Work from 11/4 and 11/7 for 16750 (#58)
    * 11/4 tuning
    * 11/7 tuning
    
    ---------
    
    Co-authored-by: FTC16750 <technototes@outlook.com>

---
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmodes/auto/ForwardBackward48.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.sixteen750.opmodes.auto;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.technototes.library.command.CommandScheduler;
+import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.library.command.WaitCommand;
+import com.technototes.library.structure.CommandOpMode;
+import com.technototes.library.util.Alliance;
+
+import org.firstinspires.ftc.sixteen750.Hardware;
+import org.firstinspires.ftc.sixteen750.Robot;
+import org.firstinspires.ftc.sixteen750.commands.PedroPathCommand;
+import org.firstinspires.ftc.sixteen750.commands.auto.Paths;
+import org.firstinspires.ftc.sixteen750.controls.DriverController;
+import org.firstinspires.ftc.sixteen750.helpers.HeadingHelper;
+import org.firstinspires.ftc.sixteen750.helpers.StartingPosition;
+
+@Autonomous(name = "ForwardBackward48", preselectTeleOp = "Dual Control")
+@SuppressWarnings("unused")
+public class ForwardBackward48 extends CommandOpMode {
+
+    public Robot robot;
+    public DriverController controls;
+    public Hardware hardware;
+
+    @Override
+    public void uponInit() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        hardware = new Hardware(hardwareMap);
+        robot = new Robot(hardware, Alliance.RED, StartingPosition.Net);
+        Paths p = new Paths(robot.follower);
+        robot.follower.setMaxPowerScaling(0.5);
+        robot.follower.setStartingPose(p.getForward48Start());
+        CommandScheduler.scheduleForState(
+            new SequentialCommandGroup(
+                new PedroPathCommand(robot.follower, p.Forward48),
+                new WaitCommand(5),
+                new PedroPathCommand(robot.follower, p.Backward48),
+                CommandScheduler::terminateOpMode
+            ),
+            OpModeState.RUN
+        );
+    }
+
+    public void uponStart() {
+        robot.prepForStart();
+    }
+
+    public void end() {
+        HeadingHelper.savePose(robot.follower.getPose());
+    }
+}
