db05f2b0a60fb46205e81a7e2d244286fad9ff62
Kevin Frei <kevinfrei@hotmail.com>
2025-11-18 09:55:15 -0800

    I tilted Mouse's camera back 14 degrees

--- LearnBot/src/main/java/org/firstinspires/ftc/learnbot/Setup.java
+++ LearnBot/src/main/java/org/firstinspires/ftc/learnbot/Setup.java
@@ -34,21 +34,4 @@ public class Setup {
         public static int AUTOTIME = 25;
         public static double TRIGGER_THRESHOLD = 0.3;
     }
-
-    public enum CameraOrientation {
-        USB_BOT_LEFT, // This is 'normal'
-        USB_UP,
-        USB_BOT_RIGHT, // This is upside down
-        USB_DOWN,
-    }
-
-    @Configurable
-    public static class Vision {
-
-        public static CameraOrientation Camera_Orientation = CameraOrientation.USB_UP;
-        public static int Green_Color_Pipeline = 0;
-        public static int AprilTag_Pipeline = 1;
-        public static int Purple_Color_Pipeline = 2;
-        public static String[] Motif = { "1", "2", "3" };
-    }
 }
