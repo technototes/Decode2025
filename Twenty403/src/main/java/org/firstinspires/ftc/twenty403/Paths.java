390ba966a5ab1f069b74813d3ddbb90776262898
Kooolpool <181432562+koolpoolo@users.noreply.github.com>
2025-11-20 17:26:02 -0800

    11/20 work

---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/Paths.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.twenty403;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+
+public class Paths {
+
+    public PathChain starttobluegoal;
+
+    public Pose getStart() {
+        return new Pose(32.671, 135.916, Math.toRadians(90));
+    }
+
+    public Pose getRStart() {
+        return new Pose(113, 135.916, Math.toRadians(90));
+    }
+
+    //blue poses
+    public Pose BlueSmall = new Pose(56.418, 8.866);
+    public Pose BlueSmallFirstBezierPoint = new Pose(74.149, 51.045);
+    public Pose BlueGoal = new Pose(23.104, 126.537);
+    public Pose intake1end = new Pose(14, 84.233);
+    public Pose intake2 = new Pose(42, 60);
+    public Pose intake2end = new Pose(8, 60);
+    public Pose intake3 = new Pose(41, 35);
+    public Pose intake3end = new Pose(8, 35);
+    public Pose move = new Pose(29.192, 49.617);
+
+    //red poses
+    public Pose Rstart = new Pose(113, 135.916);
+    public Pose Rlaunch = new Pose(105, 102.782);
+    public Pose Rintake1 = new Pose(87, 84.233);
+    public Pose Rintake1end = new Pose(125, 84.233);
+    public Pose Rintake2 = new Pose(100, 60);
+    public Pose Rintake2end = new Pose(133, 60);
+    public Pose Rintake3 = new Pose(101, 35);
+    public Pose Rintake3end = new Pose(135, 36);
+    public Pose Rmove = new Pose(115, 49.617);
+
+    public Paths(Follower follower) {
+        starttobluegoal = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                BlueSmall,
+                                BlueSmallFirstBezierPoint,
+                                BlueGoal
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(145))
+                .build();
+    }
+}
