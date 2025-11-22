0bd325ea0eded9ba10256fb8a039b85dc7ffd27d
Kevin Frei <kevinfrei@hotmail.com>
2025-11-21 22:38:28 -0800

    An attempt to make the PID for velocity more reasonable

--- LearnBot/src/main/java/org/firstinspires/ftc/learnbot/subsystems/SpinningSubsystem.java
+++ LearnBot/src/main/java/org/firstinspires/ftc/learnbot/subsystems/SpinningSubsystem.java
@@ -4,6 +4,7 @@ import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.util.Range;
+import com.technototes.library.command.CommandScheduler;
 import com.technototes.library.hardware.motor.EncodedMotor;
 import com.technototes.library.logger.Log;
 import com.technototes.library.logger.Loggable;
@@ -20,13 +21,13 @@ public class SpinningSubsystem implements Subsystem, Loggable {
     // F is unused, as we're going to have a function for it, instead
     public static PIDFCoefficients PID_VALUES = new PIDFCoefficients(1e-4, 1e-8, 1e-6, 0);
 
-    // The idea is that at full power, it spins at 6000 RPM, so the "scale" is 1/6000
+    // The idea is that at full power, if it spins at 3000 tps the "scale" is 1/3000
     // Change this to be correct for the gear ratio of your motor.
-    public static double SPIN_F_SCALE = 1.0 / 6000;
+    public static double SPIN_TO_RPM_SCALE = 1.0 / 312;
     // This should be the voltage compensation factor in F. For every 1V below "peak" voltage,
     // we'll increase the FF value by 0.1 (which simply raises power that much)
-    public static double SPIN_F_VOLTAGE_COMP = 0.1;
-    public static double PEAK_VOLTAGE = 13.0;
+    public static double INITIAL_VOLTAGE_COMP_FACTOR = 0.03;
+    public static double PEAK_VOLTAGE = 13.2;
 
     private final EncodedMotor<DcMotorEx> motor;
     private final PIDFController spinningPID;
@@ -41,22 +42,34 @@ public class SpinningSubsystem implements Subsystem, Loggable {
     @Log(name = "Motor Power")
     public double power;
 
-    public SpinningSubsystem(EncodedMotor<DcMotorEx> m, Hardware hw) {
+    @Log(name = "Motor Delta")
+    public double delta;
+
+    public Hardware hardware;
+
+    public SpinningSubsystem(EncodedMotor<DcMotorEx> m, Hardware hw, boolean dontSchedule) {
         motor = m;
+        hardware = hw;
         if (motor != null) {
             motor.coast();
         }
-        spinningPID = new PIDFController(
-            PID_VALUES,
-            target ->
-                // Do we need to take current error into account for this? I don't *think* so?
-                // If we do, you can add it as a second parameter to this lambda...
-                SPIN_F_SCALE * target + SPIN_F_VOLTAGE_COMP * Math.min(PEAK_VOLTAGE, hw.voltage())
-        );
+        spinningPID = new PIDFController(PID_VALUES);
+        if (!dontSchedule) {
+            CommandScheduler.register(this);
+        }
+    }
+
+    public SpinningSubsystem(EncodedMotor<DcMotorEx> m, Hardware hw) {
+        this(m, hw, false);
     }
 
     public void setTargetSpeed(double speed) {
         target = speed;
+        // Set the power to an initial "best guess" target
+        power =
+            speed * SPIN_TO_RPM_SCALE +
+            INITIAL_VOLTAGE_COMP_FACTOR * (PEAK_VOLTAGE - hardware.voltage());
+        setMotorPower(power);
         spinningPID.setTarget(speed);
     }
 
@@ -66,7 +79,8 @@ public class SpinningSubsystem implements Subsystem, Loggable {
 
     @Override
     public void periodic() {
-        setMotorPower(spinningPID.update(getMotorSpeed()));
+        delta = spinningPID.update(getMotorSpeed());
+        setMotorPower(power + delta);
     }
 
     public double getMotorSpeed() {
