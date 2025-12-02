63e62df682164b41085eb502443e1c9e4ebb5d9f
stedman <stedmancolinr@gmail.com>
2025-12-02 07:27:20 -0800

    improved consistency of auto made a seperate path for lever/non lever

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmodes/auto/RedSegmentedCurve.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmodes/auto/RedSegmentedCurve.java
@@ -3,9 +3,9 @@ package org.firstinspires.ftc.sixteen750.opmodes.auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.technototes.library.command.CommandScheduler;
 import com.technototes.library.command.SequentialCommandGroup;
-import com.technototes.library.command.WaitCommand;
 import com.technototes.library.structure.CommandOpMode;
 import com.technototes.library.util.Alliance;
+
 import org.firstinspires.ftc.sixteen750.Hardware;
 import org.firstinspires.ftc.sixteen750.Robot;
 import org.firstinspires.ftc.sixteen750.commands.PedroPathCommand;
@@ -42,8 +42,7 @@ public class RedSegmentedCurve extends CommandOpMode {
                 // new WaitCommand(1),
                 new PedroPathCommand(robot.follower, p.RIntake1toIntake1end),
                 // new WaitCommand(2),
-                new PedroPathCommand(robot.follower, p.RIntake1endtoLever),
-                new PedroPathCommand(robot.follower, p.RLevertoLaunch),
+                new PedroPathCommand(robot.follower, p.RIntake1endtoLaunch),
                 Paths.AutoLaunching3Balls(robot),
                 new PedroPathCommand(robot.follower, p.RLaunchtoIntake2),
                 new PedroPathCommand(robot.follower, p.RIntake2toIntake2end),
