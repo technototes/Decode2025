65d89f4ed610a39d4c654670448a4df2cba4b690
FTC16750 <technototes@outlook.com>
2025-12-06 13:02:05 -0800

    [ff97554 + fbt] Maggie - trying auto orient things - have not yet tested hold point to see if it works

---
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmodes/auto/AutoOrientTest.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.sixteen750.opmodes.auto;
+
+import static org.firstinspires.ftc.sixteen750.Setup.HardwareNames.AprilTag_Pipeline;
+
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.technototes.library.command.CommandScheduler;
+import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.library.command.WaitCommand;
+import com.technototes.library.structure.CommandOpMode;
+import com.technototes.library.util.Alliance;
+import org.firstinspires.ftc.sixteen750.Hardware;
+import org.firstinspires.ftc.sixteen750.Robot;
+import org.firstinspires.ftc.sixteen750.Setup;
+import org.firstinspires.ftc.sixteen750.commands.LLSetup;
+import org.firstinspires.ftc.sixteen750.commands.PedroPathCommand;
+import org.firstinspires.ftc.sixteen750.commands.TeleCommands;
+import org.firstinspires.ftc.sixteen750.commands.auto.Paths;
+import org.firstinspires.ftc.sixteen750.controls.DriverController;
+import org.firstinspires.ftc.sixteen750.helpers.HeadingHelper;
+import org.firstinspires.ftc.sixteen750.helpers.StartingPosition;
+import org.firstinspires.ftc.sixteen750.subsystems.LimelightSubsystem;
+
+@Autonomous(name = "AutoOrientTest", preselectTeleOp = "Dual Control")
+@SuppressWarnings("unused")
+public class AutoOrientTest extends CommandOpMode {
+
+    public Robot robot;
+    public DriverController controls;
+    public Hardware hardware;
+    public LimelightSubsystem ls;
+    private Limelight3A limelight;
+
+    @Override
+    public void uponInit() {
+        hardware = new Hardware(hardwareMap);
+        robot = new Robot(hardware, Alliance.RED, StartingPosition.Net);
+        ls = robot.limelightSubsystem;
+        Paths p = new Paths(robot.follower);
+        robot.follower.setStartingPose(new Pose(80, 35, 45));
+        if (Setup.Connected.LIMELIGHTSUBSYSTEM) {
+            limelight = hardware.limelight;
+            limelight.setPollRateHz(100);
+
+            telemetry.setMsTransmissionInterval(11);
+
+            limelight.pipelineSwitch(AprilTag_Pipeline);
+            CommandScheduler.register(robot.limelightSubsystem);
+
+            /*
+             * Starts polling for data.  If you neglect to call start(), getLatestResult() will return null.
+             */
+            limelight.start();
+            ls.getLatestResult();
+        }
+        CommandScheduler.scheduleForState(
+            new SequentialCommandGroup(
+                TeleCommands.Intake(robot),
+                TeleCommands.AltAutoOrient(robot),
+                new WaitCommand(15),
+                CommandScheduler::terminateOpMode
+            ),
+            OpModeState.RUN
+        );
+    }
+
+    public void uponStart() {
+        robot.prepForStart();
+    }
+
+    public void end() {
+        HeadingHelper.savePose(robot.follower.getPose());
+    }
+}
---
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmodes/auto/TurnToTest.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.sixteen750.opmodes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.technototes.library.command.CommandScheduler;
+import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.library.structure.CommandOpMode;
+import com.technototes.library.util.Alliance;
+import org.firstinspires.ftc.sixteen750.Hardware;
+import org.firstinspires.ftc.sixteen750.Robot;
+import org.firstinspires.ftc.sixteen750.commands.LLSetup;
+import org.firstinspires.ftc.sixteen750.commands.PedroPathCommand;
+import org.firstinspires.ftc.sixteen750.commands.TeleCommands;
+import org.firstinspires.ftc.sixteen750.commands.auto.Paths;
+import org.firstinspires.ftc.sixteen750.controls.DriverController;
+import org.firstinspires.ftc.sixteen750.helpers.HeadingHelper;
+import org.firstinspires.ftc.sixteen750.helpers.StartingPosition;
+
+@Autonomous(name = "TurnToTest", preselectTeleOp = "Dual Control")
+@SuppressWarnings("unused")
+public class TurnToTest extends CommandOpMode {
+
+    public Robot robot;
+    public DriverController controls;
+    public Hardware hardware;
+
+    @Override
+    public void uponInit() {
+        hardware = new Hardware(hardwareMap);
+        robot = new Robot(hardware, Alliance.RED, StartingPosition.Net);
+        Paths p = new Paths(robot.follower);
+        robot.follower.setStartingPose(p.getRSegmentedCurveStart());
+        CommandScheduler.register(robot.limelightSubsystem);
+        CommandScheduler.scheduleForState(
+            new SequentialCommandGroup(
+                new PedroPathCommand(robot.follower, p.RStarttoLaunch),
+                TeleCommands.TurnTo90(robot),
+                CommandScheduler::terminateOpMode
+            ),
+            OpModeState.RUN
+        );
+    }
+
+    public void uponStart() {
+        robot.prepForStart();
+    }
+
+    public void end() {
+        HeadingHelper.savePose(robot.follower.getPose());
+        robot.limelightSubsystem.LimelightTurnOff();
+    }
+}
