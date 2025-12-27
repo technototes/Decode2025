0acef562722a2be03bbe83c4c91c23cead1deae1
Kevin Frei <kevinfrei@hotmail.com>
2025-12-26 21:14:16 -0800

    Messing with some stats; Considering implementation of target-based rotation mode

--- LearnBot/src/main/java/org/firstinspires/ftc/learnbot/opmodes/TestBedTele.java
+++ LearnBot/src/main/java/org/firstinspires/ftc/learnbot/opmodes/TestBedTele.java
@@ -7,6 +7,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.MovingStatistics;
+import com.qualcomm.robotcore.util.RollingAverage;
 import com.technototes.library.control.CommandAxis;
 import com.technototes.library.control.CommandButton;
 import org.firstinspires.ftc.learnbot.Setup;
@@ -27,6 +30,8 @@ public class TestBedTele extends LinearOpMode {
     public AllianceDetection allianceDetector;
     public CommandAxis trigger;
     public CommandButton button;
+    public DcMotorEx[] motors;
+    public RollingAverage[] motorAmps;
 
     public String getAlliance() {
         return allianceDetector.get().toString();
@@ -35,13 +40,15 @@ public class TestBedTele extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // First, get the hardware
+        motors = new DcMotorEx[4];
+        motorAmps = new RollingAverage[4];
         DcMotorEx fr, fl, rr, rl;
         trigger = new CommandAxis(() -> gamepad1.left_trigger);
         button = trigger.getAsButton(triggerThreshold);
         allianceDetector = new AllianceDetection(
             this.hardwareMap,
-            Setup.HardwareNames.RED_SWITCH,
-            Setup.HardwareNames.BLUE_SWITCH
+            Setup.HardwareNames.ALLIANCE_SWITCH_RED,
+            Setup.HardwareNames.ALLIANCE_SWITCH_BLUE
         );
         if (Setup.Connected.DRIVEBASE) {
             fr = this.hardwareMap.get(DcMotorEx.class, Setup.HardwareNames.FRMOTOR);
@@ -52,6 +59,13 @@ public class TestBedTele extends LinearOpMode {
             rl.setDirection(DcMotorSimple.Direction.REVERSE);
             rr.setDirection(DcMotorSimple.Direction.FORWARD);
             fr.setDirection(DcMotorSimple.Direction.FORWARD);
+            motors[0] = fl;
+            motors[1] = fr;
+            motors[2] = rl;
+            motors[3] = rr;
+            for (int i = 0; i < 4; i++) {
+                motorAmps[i] = new RollingAverage(50);
+            }
         } else {
             fl = null;
             fr = null;
@@ -61,8 +75,10 @@ public class TestBedTele extends LinearOpMode {
 
         ptel = PanelsTelemetry.INSTANCE.getTelemetry();
         waitForStart();
+        com.qualcomm.robotcore.util.MovingStatistics loopStats = new MovingStatistics(50);
+        ElapsedTime loopTime = new ElapsedTime();
+        RollingAverage avgLoopTime = new RollingAverage(50);
         while (opModeIsActive()) {
-            sleep(5);
             if (Setup.Connected.DRIVEBASE) {
                 fl.setVelocity(
                     triggered(gamepad1.left_trigger) ? motorVelocity : 0,
@@ -74,36 +90,37 @@ public class TestBedTele extends LinearOpMode {
                 );
                 rl.setVelocity(gamepad1.left_bumper ? motorVelocity : 0, AngleUnit.RADIANS);
                 rr.setVelocity(gamepad1.right_bumper ? motorVelocity : 0, AngleUnit.RADIANS);
-                String fld = motorData(fl);
-                String frd = motorData(fr);
-                String rld = motorData(rl);
-                String rrd = motorData(rr);
+                String fld = motorData(0);
+                String frd = motorData(1);
+                String rld = motorData(2);
+                String rrd = motorData(3);
                 String redOrBlue = getAlliance();
+                double lps = 1.0 / loopTime.seconds();
+                loopTime.reset();
+                loopStats.add(lps);
                 telemetry.addData("Alliance", redOrBlue);
                 telemetry.addData("FL", fld);
                 telemetry.addData("FR", frd);
                 telemetry.addData("RL", rld);
                 telemetry.addData("RR", rrd);
-                telemetry.addData("leftX", gamepad1.left_stick_x);
-                telemetry.addData("leftY", gamepad1.left_stick_y);
-                telemetry.addData("rightX", gamepad1.right_stick_x);
-                telemetry.addData("rightY", gamepad1.right_stick_y);
-                telemetry.addData("ltrig", gamepad1.left_trigger);
-                telemetry.addData("cmd trigger", trigger.getAsDouble());
-                telemetry.addData("cmd button", button.getAsBoolean() ? "true" : "false");
+                telemetry.addData("lX", gamepad1.left_stick_x);
+                telemetry.addData("lY", gamepad1.left_stick_y);
+                telemetry.addData("rX", gamepad1.right_stick_x);
+                telemetry.addData("rY", gamepad1.right_stick_y);
+                telemetry.addData("lt", gamepad1.left_trigger);
+                telemetry.addData("LPS", String.format("%.1f avg %.1f", lps, loopStats.getMean()));
 
                 ptel.addData("Alliance", redOrBlue);
                 ptel.addData("FL", fld);
                 ptel.addData("FR", frd);
                 ptel.addData("RL", rld);
                 ptel.addData("RR", rrd);
-                ptel.addData("leftX", gamepad1.left_stick_x);
-                ptel.addData("leftY", gamepad1.left_stick_y);
-                ptel.addData("rightX", gamepad1.right_stick_x);
-                ptel.addData("rightY", gamepad1.right_stick_y);
-                ptel.addData("ltrig", gamepad1.left_trigger);
-                ptel.addData("cmd trigger", trigger.getAsDouble());
-                ptel.addData("cmd button", button.getAsBoolean() ? "true" : "false");
+                ptel.addData("lX", gamepad1.left_stick_x);
+                ptel.addData("lY", gamepad1.left_stick_y);
+                ptel.addData("rX", gamepad1.right_stick_x);
+                ptel.addData("rY", gamepad1.right_stick_y);
+                ptel.addData("lt", gamepad1.left_trigger);
+                ptel.addData("LPS", String.format("%.1f avg %.1f", lps, loopStats.getMean()));
             }
             ptel.update();
             telemetry.update();
@@ -114,10 +131,18 @@ public class TestBedTele extends LinearOpMode {
         return d > triggerThreshold;
     }
 
-    public static String motorData(DcMotorEx motor) {
+    public String motorData(int which) {
+        DcMotorEx motor = motors[which];
         double pos = motor.getCurrentPosition();
         double vel = motor.getVelocity();
         double amps = motor.getCurrent(CurrentUnit.AMPS);
-        return String.format("%.2f %.2f/s %.3fA", pos, vel, amps);
+        motorAmps[which].addNumber((int) Math.round(amps * 1000));
+        return String.format(
+            "%.2f %.2f/s %.3fA (mean: %.3fA)",
+            pos,
+            vel,
+            amps,
+            motorAmps[which].getAverage() / 1000.0
+        );
     }
 }
