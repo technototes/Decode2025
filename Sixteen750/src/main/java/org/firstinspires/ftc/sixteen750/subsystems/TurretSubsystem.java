a9f2fd8f3b24e38add00ab7880e30afd59e9484d
Kevin Frei <kevinfrei@hotmail.com>
2026-01-07 20:51:35 -0800

    Rebased & auto-formatted the code

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/subsystems/TurretSubsystem.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/subsystems/TurretSubsystem.java
@@ -8,9 +8,9 @@ import com.technototes.library.logger.Log;
 import com.technototes.library.logger.Loggable;
 import com.technototes.library.subsystem.Subsystem;
 import com.technototes.library.util.PIDFController;
-
 import org.firstinspires.ftc.sixteen750.Hardware;
 import org.firstinspires.ftc.sixteen750.Setup;
+
 @Configurable
 public class TurretSubsystem implements Subsystem, Loggable {
 
@@ -21,11 +21,14 @@ public class TurretSubsystem implements Subsystem, Loggable {
     public static double turretOffsetDegrees = 0;
     public static double TICKS_PER_REV = 384.5;
     public static double GEAR_RATIO = 4.0;
-    @Log(name = "Turret: ")
+
+    @Log(name = "Turret")
     public String TurretSubsytemInfoToDS;
+
     PIDFCoefficients turretPID = new PIDFCoefficients(0, 0, 0, 0);
     PIDFController turretPIDF = new PIDFController(turretPID);
     boolean hasHardware;
+
     public TurretSubsystem(Hardware h) {
         hasHardware = Setup.Connected.TURRETSUBSYSTEM;
         if (hasHardware) {
@@ -40,6 +43,7 @@ public class TurretSubsystem implements Subsystem, Loggable {
     private double getEncoderAngleInDegrees() {
         return (getTurretPos() / (TICKS_PER_REV * GEAR_RATIO)) * 360.0;
     }
+
     public double degreesToPosition(double angle) {
         return (angle / 360.0) * TICKS_PER_REV * GEAR_RATIO;
     }
@@ -50,93 +54,38 @@ public class TurretSubsystem implements Subsystem, Loggable {
         }
         return 0;
     }
+
     public void setTurretPos(double pos) {
         if (hasHardware) {
             turretPIDF.setTarget(pos + degreesToPosition(turretOffsetDegrees));
         }
     }
+
     private double getTurretPow() {
         if (hasHardware) {
             return turretMotor.getPower();
         }
-            return 0;
+        return 0;
     }
+
     private void setTurretPower(double power) {
         if (hasHardware) {
             power = Math.clamp(power, -1, 1);
             turretMotor.setPower(power);
         }
     }
+
     @Override
     public void periodic() {
         setTurretPower(turretPIDF.update(getTurretPos()));
         turretAngle = getEncoderAngleInDegrees();
         turretPow = getTurretPow();
         turretTicks = getTurretPos();
-        TurretSubsytemInfoToDS = String.format("Angle: %.1f Power: %.2f Pos: %i", getEncoderAngleInDegrees(), getTurretPow(), getTurretPos());
-
-
-    }
-}
-
-package org.firstinspires.ftc.sixteen750.subsystems;
-
-import com.bylazar.configurables.annotations.Configurable;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
-import com.technototes.library.hardware.motor.EncodedMotor;
-import com.technototes.library.logger.Loggable;
-import com.technototes.library.subsystem.Subsystem;
-import com.technototes.library.util.PIDFController;
-import org.firstinspires.ftc.sixteen750.Hardware;
-import org.firstinspires.ftc.sixteen750.Setup;
-
-@Configurable
-public class TurretSubsystem implements Subsystem, Loggable {
-
-    public EncodedMotor<DcMotorEx> turretMotor;
-    public static double turretAngle = 0;
-    public static double turretTicks = 0;
-    public static double turretPow = 0;
-    public static double TICKS_PER_REV = 1440;
-    double GEAR_RATIO = 1.0;
-    PIDFCoefficients turretPID = new PIDFCoefficients(0, 0, 0, 0);
-    PIDFController turretPIDF = new PIDFController(turretPID);
-    boolean hasHardware;
-
-    public TurretSubsystem(Hardware h) {
-        turretMotor = h.turretMotor;
-        hasHardware = true;
-    }
-
-    private double getEncoderAngle() {
-        return (getTurretPos() / (TICKS_PER_REV * GEAR_RATIO)) * 360.0;
-    }
-
-    public double angleToPosition(double angle) {
-        return (angle / 360.0) * TICKS_PER_REV * GEAR_RATIO;
-    }
-
-    private double getTurretPos() {
-        return turretMotor.getSensorValue();
-    }
-
-    private double getTurretPow() {
-        return turretMotor.getPower();
-    }
-
-    private void setTurretPower(double power) {
-        if (hasHardware) {
-            power = Math.clamp(power, -1, 1);
-            turretMotor.setPower(power);
-        }
-    }
-
-    @Override
-    public void periodic() {
-        setTurretPower(turretPIDF.update(getTurretPos()));
-        turretAngle = getEncoderAngle();
-        turretPow = getTurretPow();
-        turretTicks = getTurretPos();
+        TurretSubsytemInfoToDS = String.format(
+            "Angle: %.1f Power: %.2f Pos: %i",
+            getEncoderAngleInDegrees(),
+            getTurretPow(),
+            getTurretPos()
+        );
     }
 }
