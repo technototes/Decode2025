e78cffba4cd6423b4b995085cde516e06d57cf8b
koolpoolo <181432562+koolpoolo@users.noreply.github.com>
2026-01-13 17:37:39 -0800

    periodic and gamepad input dont work?

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/controls/TestingController.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/controls/TestingController.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.sixteen750.controls;
 
+import com.bylazar.configurables.annotations.Configurable;
+import com.technototes.library.command.Command;
 import com.technototes.library.control.CommandButton;
 import com.technototes.library.control.CommandGamepad;
 import org.firstinspires.ftc.sixteen750.Robot;
 import org.firstinspires.ftc.sixteen750.Setup;
 import org.firstinspires.ftc.sixteen750.commands.TeleCommands;
-
 public class TestingController {
 
     public Robot robot;
@@ -13,6 +14,7 @@ public class TestingController {
 
     public CommandButton motorPowerButton;
     public CommandButton motorVelocityButton;
+    public CommandButton TurretMoveToPose;
 
     public TestingController(CommandGamepad g, Robot r) {
         robot = r;
@@ -22,13 +24,15 @@ public class TestingController {
     }
 
     private void AssignNamedControllerButton() {
-        motorPowerButton = gamepad.ps_circle;
-        motorVelocityButton = gamepad.ps_triangle;
+//        motorPowerButton = gamepad.ps_circle;
+//        motorVelocityButton = gamepad.ps_triangle;
+        TurretMoveToPose = gamepad.ps_cross;
     }
 
     private void bindButtonControls() {
-        motorPowerButton.whenPressed(TeleCommands.MotorPowerTest(robot));
-        // motorPowerButton.whenReleased(TeleCommands.)/
-        motorVelocityButton.whenPressed(TeleCommands.MotorVelocityTest(robot));
+//        motorPowerButton.whenPressed(TeleCommands.MotorPowerTest(robot));
+//        // motorPowerButton.whenReleased(TeleCommands.)/
+//        motorVelocityButton.whenPressed(TeleCommands.MotorVelocityTest(robot));
+        TurretMoveToPose.whenPressed(Command.create(robot.turretSubsystem::turretzero));
     }
 }
