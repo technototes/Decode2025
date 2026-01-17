a40a62978a304e1fbca3f48a9545c67c16548641
FTC16750 <technototes@outlook.com>
2026-01-16 17:54:51 -0800

    1/16 - auto tuning - looks good!

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmodes/auto/BlueNearFirstandSecondLever.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmodes/auto/BlueNearFirstandSecondLever.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.sixteen750.opmodes.auto;
 
+import static org.firstinspires.ftc.sixteen750.Setup.HardwareNames.AprilTag_Pipeline;
+
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.technototes.library.command.CommandScheduler;
 import com.technototes.library.command.SequentialCommandGroup;
@@ -8,6 +12,7 @@ import com.technototes.library.structure.CommandOpMode;
 import com.technototes.library.util.Alliance;
 import org.firstinspires.ftc.sixteen750.Hardware;
 import org.firstinspires.ftc.sixteen750.Robot;
+import org.firstinspires.ftc.sixteen750.Setup;
 import org.firstinspires.ftc.sixteen750.commands.AltAutoVelocity;
 import org.firstinspires.ftc.sixteen750.commands.PedroPathCommand;
 import org.firstinspires.ftc.sixteen750.commands.TeleCommands;
@@ -15,20 +20,24 @@ import org.firstinspires.ftc.sixteen750.commands.auto.Paths;
 import org.firstinspires.ftc.sixteen750.controls.DriverController;
 import org.firstinspires.ftc.sixteen750.helpers.HeadingHelper;
 import org.firstinspires.ftc.sixteen750.helpers.StartingPosition;
+import org.firstinspires.ftc.sixteen750.subsystems.LauncherSubsystem;
 
-@Autonomous(name = "BlueNearLever2️⃣", preselectTeleOp = "Dual Control")
+@Autonomous(name = "BlueNearLever1️⃣2️⃣", preselectTeleOp = "Dual Control")
 @SuppressWarnings("unused")
-public class BlueNearSecondLever extends CommandOpMode {
+public class BlueNearFirstandSecondLever extends CommandOpMode {
 
     public Robot robot;
     public DriverController controls;
     public Hardware hardware;
+    private PanelsTelemetry panelsTelemetry;
+    private Limelight3A limelight;
 
     @Override
     public void uponInit() {
         hardware = new Hardware(hardwareMap);
         robot = new Robot(hardware, Alliance.BLUE, StartingPosition.Net);
         Paths p = new Paths(robot.follower);
+        panelsTelemetry = PanelsTelemetry.INSTANCE;
         robot.follower.setStartingPose(p.getBSegmentedCurveStart());
         CommandScheduler.scheduleForState(
             new AltAutoVelocity(robot).alongWith(
@@ -46,8 +55,12 @@ public class BlueNearSecondLever extends CommandOpMode {
                         TeleCommands.Intake(robot)
                     ),
                     new WaitCommand(0.05),
-                    //   TeleCommands.AutoLaunch2(robot),
-                    new PedroPathCommand(robot.follower, p.Intake1endtoLaunch),
+                    new PedroPathCommand(robot.follower, p.Intake1endtoLever).alongWith(
+                        TeleCommands.IntakeStop(robot)
+                    ),
+                    new WaitCommand(0.5),
+                    new PedroPathCommand(robot.follower, p.LevertoLaunch, p.power2),
+                    new WaitCommand(0.05),
                     Paths.AutoLaunching3Balls(robot),
                     new PedroPathCommand(robot.follower, p.LaunchtoIntake2),
                     new PedroPathCommand(robot.follower, p.Intake2toIntake2end, p.power).alongWith(
@@ -56,10 +69,14 @@ public class BlueNearSecondLever extends CommandOpMode {
                     new WaitCommand(0.05),
                     //TeleCommands.AutoLaunch2(robot),
 
-                    new PedroPathCommand(robot.follower, p.Intake2endtoLever, p.power2),
+                    new PedroPathCommand(robot.follower, p.Intake2endtoLever).alongWith(
+                        TeleCommands.IntakeStop(robot)
+                    ),
+                    new WaitCommand(0.5),
+                    new PedroPathCommand(robot.follower, p.LevertoLaunch),
+                    new WaitCommand(0.05),
                     Paths.AutoLaunching3Balls(robot),
                     new PedroPathCommand(robot.follower, p.LaunchtoIntake3),
-                    new WaitCommand(0.05),
                     new PedroPathCommand(robot.follower, p.Intake3toIntake3end, p.power).alongWith(
                         TeleCommands.Intake(robot)
                     ),
@@ -71,48 +88,59 @@ public class BlueNearSecondLever extends CommandOpMode {
                     new PedroPathCommand(robot.follower, p.LaunchtoEnd, p.power2),
                     TeleCommands.IntakeStop(robot),
                     CommandScheduler::terminateOpMode
-
-                    //        CommandScheduler.scheduleForState(
-                    //            new SequentialCommandGroup(
-                    //                TeleCommands.GateUp(robot),
-                    //                TeleCommands.AutoLaunch1(robot),
-                    //                TeleCommands.Intake(robot),
-                    //                TeleCommands.HoodUp(robot),
-                    //                new PedroPathCommand(robot.follower, p.StarttoLaunch),
-                    //                Paths.AutoLaunching3Balls(robot),
-                    //                // new WaitCommand(0.5),
-                    //                new PedroPathCommand(robot.follower, p.LaunchtoIntake1),
-                    //                // new WaitCommand(1),
-                    //                new PedroPathCommand(robot.follower, p.Intake1toIntake1end, 0.8),
-                    //                new PedroPathCommand(robot.follower, p.Intake1endtoLever),
-                    //                new WaitCommand(1),
-                    //                // new WaitCommand(2),
-                    //                new PedroPathCommand(robot.follower, p.LevertoLaunch),
-                    //                Paths.AutoLaunching3Balls(robot),
-                    //                new PedroPathCommand(robot.follower, p.LaunchtoIntake2),
-                    //                new PedroPathCommand(robot.follower, p.Intake2toIntake2end, p.power),
-                    //                new PedroPathCommand(robot.follower, p.Intake2endtoLaunch),
-                    //                Paths.AutoLaunching3Balls(robot),
-                    //                new PedroPathCommand(robot.follower, p.LaunchtoIntake3),
-                    //                new WaitCommand(2),
-                    //                new PedroPathCommand(robot.follower, p.Intake3toIntake3end, p.power),
-                    //                new PedroPathCommand(robot.follower, p.Intake3endtoLaunch),
-                    //                Paths.AutoLaunching3Balls(robot),
-                    //                new PedroPathCommand(robot.follower, p.LaunchtoEnd, p.power),
-                    //                TeleCommands.StopLaunch(robot),
-                    //                TeleCommands.IntakeStop(robot),
-                    //                CommandScheduler::terminateOpMode
                 )
             ),
             OpModeState.RUN
         );
+        if (Setup.Connected.LIMELIGHTSUBSYSTEM) {
+            limelight = hardware.limelight;
+            limelight.setPollRateHz(100);
+
+            telemetry.setMsTransmissionInterval(11);
+
+            limelight.pipelineSwitch(AprilTag_Pipeline);
+            CommandScheduler.register(robot.limelightSubsystem);
+
+            /*
+             * Starts polling for data.  If you neglect to call start(), getLatestResult() will return null.
+             */
+            limelight.start();
+        }
+        if (Setup.Connected.LAUNCHERSUBSYSTEM) {
+            CommandScheduler.register(robot.launcherSubsystem);
+        }
     }
 
     public void uponStart() {
         robot.prepForStart();
     }
 
+    public void runLoop() {
+        panelsTelemetry
+            .getTelemetry()
+            .addData(
+                "currentLaunchVelocity",
+                String.valueOf(LauncherSubsystem.currentLaunchVelocity)
+            );
+        panelsTelemetry
+            .getTelemetry()
+            .addData("launcherError", String.valueOf(LauncherSubsystem.err));
+        panelsTelemetry
+            .getTelemetry()
+            .addData("launcherTargetVelocity", String.valueOf(LauncherSubsystem.targetSpeed));
+        panelsTelemetry
+            .getTelemetry()
+            .addData("launcher1Current", String.valueOf(LauncherSubsystem.launcher1Current));
+        panelsTelemetry
+            .getTelemetry()
+            .addData("launcher2Current", String.valueOf(LauncherSubsystem.launcher2Current));
+        panelsTelemetry.getTelemetry().update(telemetry);
+    }
+
     public void end() {
         HeadingHelper.savePose(robot.follower.getPose());
+        if (Setup.Connected.LIMELIGHTSUBSYSTEM) {
+            limelight.stop();
+        }
     }
 }
