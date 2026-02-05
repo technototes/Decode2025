a16d2da97bd2b1db08f2489d7f6e9c5a9fdca8e9
Kevin Frei <kevinfrei@hotmail.com>
2026-02-04 21:29:20 -0800

    Fixed the build break due to me changing TechnoLib and deleting local HeadingHelper classes

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmodes/auto/Red15BallNear.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmodes/auto/Red15BallNear.java
@@ -10,6 +10,7 @@ import com.technototes.library.command.SequentialCommandGroup;
 import com.technototes.library.command.WaitCommand;
 import com.technototes.library.structure.CommandOpMode;
 import com.technototes.library.util.Alliance;
+import com.technototes.library.util.HeadingHelper;
 import org.firstinspires.ftc.sixteen750.Hardware;
 import org.firstinspires.ftc.sixteen750.Robot;
 import org.firstinspires.ftc.sixteen750.Setup;
@@ -18,7 +19,6 @@ import org.firstinspires.ftc.sixteen750.commands.PedroPathCommand;
 import org.firstinspires.ftc.sixteen750.commands.TeleCommands;
 import org.firstinspires.ftc.sixteen750.commands.auto.Paths;
 import org.firstinspires.ftc.sixteen750.controls.DriverController;
-import org.firstinspires.ftc.sixteen750.helpers.HeadingHelper;
 import org.firstinspires.ftc.sixteen750.helpers.StartingPosition;
 import org.firstinspires.ftc.sixteen750.subsystems.LauncherSubsystem;
 
