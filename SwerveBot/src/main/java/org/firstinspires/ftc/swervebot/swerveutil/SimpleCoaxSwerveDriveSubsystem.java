6a33c41051be655bc788f81ec10e10315f6b0270
FTC16750 <technototes@outlook.com>
2026-07-15 16:49:39 -0700

    swerve

--- SwerveBot/src/main/java/org/firstinspires/ftc/swervebot/swerveutil/SimpleCoaxSwerveDriveSubsystem.java
+++ SwerveBot/src/main/java/org/firstinspires/ftc/swervebot/swerveutil/SimpleCoaxSwerveDriveSubsystem.java
@@ -14,22 +14,21 @@ import com.technototes.library.logger.Loggable;
 import com.technototes.library.subsystem.Subsystem;
 import com.technototes.library.util.MathUtils;
 import com.technototes.library.util.PIDFController;
-
+import java.util.Arrays;
+import java.util.OptionalDouble;
 import org.firstinspires.ftc.swervebot.Hardware;
 import org.firstinspires.ftc.swervebot.Setup;
 
-import java.util.Arrays;
-import java.util.OptionalDouble;
-
 @Configurable
 public class SimpleCoaxSwerveDriveSubsystem implements Loggable, Subsystem {
+
     //fr,fl,rl,rr
     DcMotorEx[] drive = new DcMotorEx[4];
     //fr,fl,rl,rr
     CRServo[] swervo = new CRServo[4];
     //fr,fl,rl,rr
     AbsoluteAnalogEncoder[] swervoencs = new AbsoluteAnalogEncoder[4];
-    public static PIDFCoefficients swervoPID = new PIDFCoefficients(0.3,0,0.003,0);
+    public static PIDFCoefficients swervoPID = new PIDFCoefficients(0.3, 0, 0.003, 0);
     PIDFController[] swervoPIDF = new PIDFController[4];
 
     boolean hasHardware;
@@ -40,79 +39,115 @@ public class SimpleCoaxSwerveDriveSubsystem implements Loggable, Subsystem {
     double trackWidth = 10.85;
     double driveLength = 10.69;
     double R = Math.sqrt((trackWidth * trackWidth) + (driveLength * driveLength));
+
     @Log.Number(name = "X: ")
     public static double forwardInput = 0;
+
     @Log.Number(name = "Y: ")
     public static double strafeInput = 0;
+
     @Log.Number(name = "R: ")
     public static double rotationInput = 0;
+
     @Log.Number(name = "rNeg: ")
     public static double rNeg = 0;
+
     @Log.Number(name = "A: ")
     public static double A = 0;
+
     @Log.Number(name = "B: ")
     public static double B = 0;
+
     @Log.Number(name = "C: ")
     public static double C = 0;
+
     @Log.Number(name = "D: ")
     public static double D = 0;
+
     @Log.Number(name = "E: ")
     public static double E = 0;
+
     @Log.Number(name = "F: ")
     public static double F = 0;
+
     @Log.Number(name = "G: ")
     public static double G = 0;
+
     @Log.Number(name = "H: ")
     public static double H = 0;
+
     @Log.Number(name = "Enc0 (deg): ")
     public static double enc0Deg = 0;
+
     @Log.Number(name = "Enc1 (deg): ")
     public static double enc1Deg = 0;
+
     @Log.Number(name = "Enc2 (deg): ")
     public static double enc2Deg = 0;
+
     @Log.Number(name = "Enc3 (deg): ")
     public static double enc3Deg = 0;
 
     @Log.Number(name = "Err0 (deg): ")
     public static double err0Deg = 0;
+
     @Log.Number(name = "Err1 (deg): ")
     public static double err1Deg = 0;
+
     @Log.Number(name = "Err2 (deg): ")
     public static double err2Deg = 0;
+
     @Log.Number(name = "Err3 (deg): ")
     public static double err3Deg = 0;
+
     @Log.Number(name = "Target0 (deg): ")
     public static double target0Deg = 0;
+
     @Log.Number(name = "Target1 (deg): ")
     public static double target1Deg = 0;
+
     @Log.Number(name = "Target2 (deg): ")
     public static double target2Deg = 0;
+
     @Log.Number(name = "Target3 (deg): ")
     public static double target3Deg = 0;
+
     @Log.Number(name = "Power0: ")
     public static double power0 = 0;
+
     @Log.Number(name = "Power1: ")
     public static double power1 = 0;
+
     @Log.Number(name = "Power2: ")
     public static double power2 = 0;
+
     @Log.Number(name = "Power3: ")
     public static double power3 = 0;
+
     @Log.Number(name = "Raw0 (deg): ")
     public static double raw0 = 0;
+
     @Log.Number(name = "Raw1 (deg): ")
     public static double raw1 = 0;
+
     @Log.Number(name = "Raw2 (deg): ")
     public static double raw2 = 0;
+
     @Log.Number(name = "Raw3 (deg): ")
     public static double raw3 = 0;
+
     @Log.Number(name = "AngleDiff0: ")
     public static double diff0 = 0;
+
     @Log.Number(name = "AngleDiff1: ")
     public static double diff1 = 0;
+
     @Log.Number(name = "AngleDiff2: ")
     public static double diff2 = 0;
+
     @Log.Number(name = "AngleDiff3: ")
     public static double diff3 = 0;
+
     double[] unfilteredWheelPowers = new double[4];
 
     OptionalDouble maxPower = OptionalDouble.of(0.0);
@@ -121,7 +156,6 @@ public class SimpleCoaxSwerveDriveSubsystem implements Loggable, Subsystem {
     double[] angleDifferences = new double[4];
     IGyro imu;
     double robotHeading;
-
 
     public SimpleCoaxSwerveDriveSubsystem(Hardware hw) {
         hasHardware = Setup.Connected.SWERVESUBSYSTEM;
@@ -147,7 +181,6 @@ public class SimpleCoaxSwerveDriveSubsystem implements Loggable, Subsystem {
                 drive[i].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             }
             imu = hw.imu;
-
         } else {
             drive = null;
             swervo = null;
@@ -160,12 +193,10 @@ public class SimpleCoaxSwerveDriveSubsystem implements Loggable, Subsystem {
     @Override
     public void periodic() {
         if (hasHardware) {
-
             power0 = wheelPowers[0];
             power1 = wheelPowers[1];
             power2 = wheelPowers[2];
             power3 = wheelPowers[3];
-
         }
     }
 
@@ -174,32 +205,38 @@ public class SimpleCoaxSwerveDriveSubsystem implements Loggable, Subsystem {
             crServo.setPower(power);
         }
     }
+
     public void setSwervosPow() {
         if (hasHardware) {
             swervo[0].setPower(1);
             swervo[1].setPower(1);
             swervo[2].setPower(1);
             swervo[3].setPower(1);
-
         }
     }
+
     public Command setSwervosCmd() {
         if (hasHardware) {
-            return () -> new ParallelCommandGroup(() -> swervo[0].setPower(1),
-                () -> swervo[1].setPower(1),
-                () -> swervo[2].setPower(1),
-                () -> swervo[3].setPower(1));
-
+            return () ->
+                new ParallelCommandGroup(
+                    () -> swervo[0].setPower(1),
+                    () -> swervo[1].setPower(1),
+                    () -> swervo[2].setPower(1),
+                    () -> swervo[3].setPower(1)
+                );
         }
         return null;
     }
+
     public Command stopSwervosCmd() {
         if (hasHardware) {
-            return () -> new ParallelCommandGroup(() -> swervo[0].setPower(0),
-                () -> swervo[1].setPower(0),
-                () -> swervo[2].setPower(0),
-                () -> swervo[3].setPower(0));
-
+            return () ->
+                new ParallelCommandGroup(
+                    () -> swervo[0].setPower(0),
+                    () -> swervo[1].setPower(0),
+                    () -> swervo[2].setPower(0),
+                    () -> swervo[3].setPower(0)
+                );
         }
         return null;
     }
@@ -211,6 +248,7 @@ public class SimpleCoaxSwerveDriveSubsystem implements Loggable, Subsystem {
             }
         }
     }
+
     private void setSwervoPos(PIDFController swervoPIDF, double pos) {
         if (hasHardware) {
             swervoPIDF.setTarget(pos);
@@ -234,28 +272,38 @@ public class SimpleCoaxSwerveDriveSubsystem implements Loggable, Subsystem {
             B = strafeInput + rotationInput * (driveLength / R);
             C = forwardInput - rotationInput * (trackWidth / R);
             D = forwardInput + rotationInput * (trackWidth / R);
-            rNeg = -rotationInput;
-            E = strafeInput - rNeg * (driveLength / R);
-            F = strafeInput + rNeg * (driveLength / R);
-            G = forwardInput - rNeg * (trackWidth / R);
-            H = forwardInput + rNeg * (trackWidth / R);
-            unfilteredWheelPowers[0] = Math.sqrt((F * F) + (G * G)) * (r != 0 ? (-rotationInput / r ) : 1);
+            //            rNeg = -rotationInput;
+            //            E = strafeInput - rNeg * (driveLength / R);
+            //            F = strafeInput + rNeg * (driveLength / R);
+            //            G = forwardInput - rNeg * (trackWidth / R);
+            //            H = forwardInput + rNeg * (trackWidth / R);
+            unfilteredWheelPowers[0] = Math.sqrt(B * B + C * C); //Math.sqrt((F * F) + (G * G)) * (r != 0 ? (-rotationInput / r ) : 1);
             unfilteredWheelPowers[1] = Math.sqrt((B * B) + (D * D));
-            unfilteredWheelPowers[2] = Math.sqrt((E * E) + (H * H)) * (r !=0 ? (-rotationInput / r ) : 1);
+            unfilteredWheelPowers[2] = Math.sqrt(A * A + D * D); //Math.sqrt((E * E) + (H * H)) * (r !=0 ? (-rotationInput / r ) : 1);
             unfilteredWheelPowers[3] = Math.sqrt((A * A) + (C * C));
             maxPower = Arrays.stream(unfilteredWheelPowers).max();
             for (int i = 0; i < 4; i++) {
                 wheelPowers[i] = maxPower.getAsDouble() > 1
-                        ? unfilteredWheelPowers[i] / maxPower.getAsDouble()
-                        : unfilteredWheelPowers[i];
+                    ? unfilteredWheelPowers[i] / maxPower.getAsDouble()
+                    : unfilteredWheelPowers[i];
             }
-            wheelAngles[0] = C == 0 && B == 0 ? 0 : MathUtils.normalizeDeltaRadians(Math.atan2(C, B));
-            wheelAngles[1] = D == 0 && B == 0 ? 0 : MathUtils.normalizeDeltaRadians(Math.atan2(D, B));
-            wheelAngles[2] = D == 0 && A == 0 ? 0 : MathUtils.normalizeDeltaRadians(Math.atan2(D, A));
-            wheelAngles[3] = C == 0 && A == 0 ? 0 : MathUtils.normalizeDeltaRadians(Math.atan2(C, A));
+            wheelAngles[0] = C == 0 && B == 0
+                ? 0
+                : MathUtils.normalizeDeltaRadians(Math.atan2(C, B));
+            wheelAngles[1] = D == 0 && B == 0
+                ? 0
+                : MathUtils.normalizeDeltaRadians(Math.atan2(D, B));
+            wheelAngles[2] = D == 0 && A == 0
+                ? 0
+                : MathUtils.normalizeDeltaRadians(Math.atan2(D, A));
+            wheelAngles[3] = C == 0 && A == 0
+                ? 0
+                : MathUtils.normalizeDeltaRadians(Math.atan2(C, A));
             for (int i = 0; i < 4; i++) {
                 swervopos[i] = swervoencs[i].getCurrentPosition();
-                angleDifferences[i] = MathUtils.normalizeDeltaRadians(wheelAngles[i] - swervopos[i]);
+                angleDifferences[i] = MathUtils.normalizeDeltaRadians(
+                    wheelAngles[i] - swervopos[i]
+                );
                 if (Math.abs(angleDifferences[i]) > Math.PI / 2) {
                     wheelAngles[i] += Math.PI;
                     wheelPowers[i] *= -1;
@@ -288,36 +336,37 @@ public class SimpleCoaxSwerveDriveSubsystem implements Loggable, Subsystem {
                 swervoPIDF[i].setTarget(wheelAngles[i]);
                 setSwervoPow(swervo[i], -swervoPIDF[i].update(swervopos[i]));
                 drive[i].setPower(wheelPowers[i]);
-
             }
         }
     }
+
     private double DeadZoneScale(double d) {
         // Okay, we want a small dead zone in the middle of the stick, but that also means that
         // you can't have a value any smaller than that value, so instead, we're going to scale
         // the value after compensating for the dead zone
-            // If the value is inside the dead zone, just make it zero
-            if (Math.abs(d) <= Setup.OtherSettings.STICK_DEAD_ZONE) {
-                return 0.0;
-            }
-            // If the value is outside the dead zone, scale it
-            return (
-                    (d - Math.copySign(Setup.OtherSettings.STICK_DEAD_ZONE, d)) /
-                            (1.0 - Setup.OtherSettings.STICK_DEAD_ZONE)
-            );
-
+        // If the value is inside the dead zone, just make it zero
+        if (Math.abs(d) <= Setup.OtherSettings.STICK_DEAD_ZONE) {
+            return 0.0;
+        }
+        // If the value is outside the dead zone, scale it
+        return (
+            (d - Math.copySign(Setup.OtherSettings.STICK_DEAD_ZONE, d)) /
+            (1.0 - Setup.OtherSettings.STICK_DEAD_ZONE)
+        );
     }
+
     public void setAllModuleAngles(double angle) {
         if (hasHardware) {
             for (int i = 0; i < 4; i++) {
                 swervoPIDF[i].setTarget(angle);
             }
         }
-
     }
+
     public Command setAll180Cmd() {
         return () -> setAllModuleAngles(Math.PI);
     }
+
     public void setAllModules180() {
         if (hasHardware) {
             for (int i = 0; i < 4; i++) {
